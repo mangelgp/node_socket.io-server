d61ebb43fbc7973bf97ef7284dd74a93e521e7c3
Miguel Angel Gomez Perez <mangel1098@outlook.com>
2020-09-15 17:52:08 -0500

    leccion 36

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,23 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Hillsong') );
+bands.addBand( new Band('Un Corazon') );
+bands.addBand( new Band('IBI') );
+bands.addBand( new Band('Zoe') );
+bands.addBand( new Band('Leave') );
+
+console.log(bands);
+
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -11,6 +26,12 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
         io.emit('mensaje', {admin: 'Nuevo mensaje'})
+    });
+
+    client.on('vote-band', (payload) => {
+        console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('bands', bands.getBands());
     });
 
     client.on('emitir-mensaje',(payload) => {
